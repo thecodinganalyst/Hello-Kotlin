c6d689dba4149c91286a6ccdb71ea240a1245610
Dennis Cai <dennis@thecodinganalyst.com>
2021-01-25 11:27:10 +0800

    String Literals

---
+++ 01_Getting_Started/code/0104_GS_Raw_Strings.kt
@@ -0,0 +1,21 @@
+fun main(){
+	val text = """
+		This is a multiline string of text
+		enclosed within 2 sets of triple double quotes 
+		Escaped characters like \n will be shown as literal strings
+		All white spaces, like tabs and newlines, will be preserved
+	"""
+
+	println(text)
+
+	val text2 = """
+		|However, you can use margin prefix
+		|and append the trimMargin() function after the string to 
+		|trim off the excess white spaces
+		|The pipe character is the default
+		|but you can use any other characters 
+		|pass in the desired character in the trimMargin()
+	""".trimMargin()
+
+	println(text2)
+}
